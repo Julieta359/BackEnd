d7ca273ae3d30feffdd825c24ba5dc44a24124a0
Julieta359 <119441514+Julieta359@users.noreply.github.com>
2023-03-06 20:32:58 -0300

    CORS X3

--- src/main/java/com/portfolio/jds/Controller/CEducacion.java
+++ src/main/java/com/portfolio/jds/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://frontjds.web.app")
+@CrossOrigin(origins = "*")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
