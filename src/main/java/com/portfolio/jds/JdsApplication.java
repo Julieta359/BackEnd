5e3a650677785388cabd2cedb0fe08604ad7ccf7
Julieta359 <119441514+Julieta359@users.noreply.github.com>
2023-03-06 15:16:45 -0300

    Carpeta BackEnd

---
+++ src/main/java/com/portfolio/jds/JdsApplication.java
@@ -0,0 +1,18 @@
+package com.portfolio.jds;
+
+import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+public class JdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdsApplication.class, args);
+    }
+
+}
